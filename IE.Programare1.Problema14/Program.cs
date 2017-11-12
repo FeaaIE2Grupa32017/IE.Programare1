106cc91df1008fc5d9591dd0ad7de7bcc1c2e012
Viorel Iacoban <viorel.iacoban@gmail.com>
2017-11-12 21:23:52 +0200

    Seminar 6

---
+++ IE.Programare1.Problema14/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IE.Programare1.Problema14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int rest;
+            int suma = 0;
+            sbyte[] cifre = new sbyte[0];
+            while(n > 0)
+            {
+                rest = n % 10;
+                suma += rest;
+                Array.Resize(ref cifre, cifre.Length + 1);
+                cifre[cifre.Length - 1] = (sbyte)rest;
+                n = n / 10;
+            }
+
+            string mesaj = "Cifrele lui n sunt: ";
+
+            for(int i = 0; i < cifre.Length; i++)
+            {
+                mesaj += cifre[i] + " ";
+            }
+
+            Console.WriteLine(mesaj);
+            Console.WriteLine("Suma este: " + suma);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IE.Programare1.Problema4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IE.Programare1.Problema4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti n");
+            int n = int.Parse(Console.ReadLine());
+            float[] valori = new float[n];
+            for(int i = 0; i < valori.Length; i++)
+            {
+                Console.WriteLine("Introduceti valoarea de la pozitia: " + i);
+                valori[i] = float.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(string.Join(",", valori));
+            for(int i = 0; i < valori.Length - 1; i += 2)
+            {
+                float suma = valori[i] + valori[i + 1];
+                Array.Resize(ref valori, valori.Length + 1);
+                int j = valori.Length;
+                while(j > i + 1)
+                {
+                    j--;
+                    valori[j] = valori[j - 1];
+                }
+                valori[i + 1] = suma;
+            }
+            Console.WriteLine(string.Join(",", valori));
+
+
+            Console.ReadKey();
+        }
+    }
+}
